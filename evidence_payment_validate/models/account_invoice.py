794b9ca3d0703163941130e4f92bfe55587eaf30
Esther_Cisneros <mariaesther11@live.com.mx>
2018-08-29 11:41:01 -0500

    modulo nuevo para validar pago de facturas Galbo

--- evidence_payment_validate/models/account_invoice.py
+++ evidence_payment_validate/models/account_invoice.py
@@ -2,20 +2,20 @@
 # Copyright 2018, Esther Cisneros
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import _, api, fields, models
+from openerp import _, api,exceptions, _, fields, models
 
 class AccountInvoice(models.Model):
     # _name = 'account.payment'
     _inherit = 'account.invoice'
 
     evidence_status = fields.Selection(
-    	[('received', _('Received')),
-    	 ('not_received', _('Not Received'))],
-    	string=_('Evidencia Galbo'),
-    	default='not_received'
+        [('received', _('Received')),
+         ('not_received', _('Not Received'))],
+        string=_('Evidencia Galbo'),
+        default='not_received'
     )
     @api.multi
     def button_invoice_evidence (self):
-    	for rec in self:
-    		rec.write({'evidence_status': 'received'})
-    	return True
+        for rec in self:
+            rec.write({'evidence_status': 'received'})
+        return True
--- evidence_payment_validate/wizards/account_invoice_wizard_evi.py
+++ evidence_payment_validate/wizards/account_invoice_wizard_evi.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import _, api, fields, models
+from openerp.exceptions import UserError, ValidationError
 
 class AccountInvoiceWizardEvi(models.TransientModel):
     _name = 'account.invoice.wizard.evi'
@@ -12,5 +13,9 @@ class AccountInvoiceWizardEvi(models.TransientModel):
     	invoice_obj = self.env['account.invoice']
     	active_ids = self._context.get('active_ids', []) or []
     	invoice_var = invoice_obj.browse(active_ids)
+    	if any (inv.state != 'open' for inv in invoice_var):
+    		raise UserError(_("Solo puedes aprobar evidencias para facturas abiertas"))
+    	if any(inv.partner_id.id != 2503 for inv in invoice_var):
+    		raise UserError(_("Solo puedes aprobar evidencias de facturas de Transportes Galbo"))	
     	for inv in invoice_var:
     		inv.evidence_status = 'received'
