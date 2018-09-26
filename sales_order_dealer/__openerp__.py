d1cb6051dd71e3e84a3f49b5027b0fa1d78a92f4
Esther_Cisneros <mariaesther11@live.com.mx>
2018-09-26 16:45:34 -0500

    Actualizacion mosulo sales_order_dealer

--- sales_order_dealer/__openerp__.py
+++ sales_order_dealer/__openerp__.py
@@ -2,8 +2,8 @@
 # © <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Sale Order Dealer",
-    "summary": "Sale Order Dealer",
+    "name": "Sale Order Dealer.",
+    "summary": "Sale Order Dealer.",
     "version": "9.0.1.0.0",
     "category": "Uncategorized",
     "website": "https://odoo-community.org/",
