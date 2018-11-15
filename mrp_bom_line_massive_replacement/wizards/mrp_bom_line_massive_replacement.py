8aaa5ecc3b0796c4b8699174f243bdd739dc229d
cesar barron <cesar.barron@gmail.com>
2018-11-15 13:35:54 -0600

    Loggin

--- mrp_bom_line_massive_replacement/wizards/mrp_bom_line_massive_replacement.py
+++ mrp_bom_line_massive_replacement/wizards/mrp_bom_line_massive_replacement.py
@@ -2,8 +2,10 @@
 # © <YEAR(S)> <AUTHOR(S)>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from openerp import _, api, fields, models
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class MrpBomLineMassiveReplacement(models.TransientModel):
     _name = "mrp.bom.line.massive.replacement"
@@ -22,8 +24,12 @@ class MrpBomLineMassiveReplacement(models.TransientModel):
 
     @api.multi
     def process(self):
+        _logger.warning(
+            _('GEB - Entrando al metodo'))
         bom_line_obj = self.env['mrp.bom.line']
         for replacement in self:
+            _logger.warning(
+                _('GEB - limite %s') % str(replacement.limit))
             if replacement.limit > 0:
                 bom_line = bom_line_obj.search(
                     [('product_id', '=', replacement.product_id.id)],
@@ -32,12 +38,22 @@ class MrpBomLineMassiveReplacement(models.TransientModel):
                 bom_line = bom_line_obj.search(
                     [('product_id', '=', replacement.product_id.id)])
             done_ids = []
+            _logger.warning(
+                _('GEB - num reemp %s') % str(len(bom_line)))
             for line in bom_line:
                 line.product_id = replacement.new_product_id.id
                 if line.bom_id.id in done_ids:
                     continue
+                _logger.warning(
+                    _('GEB - ACTUAL %s') % line.bom_id.product_id.name)
                 done_ids.append(line.bom_id.id)
+
+            _logger.warning(
+                _('GEB - Entrando revaluar'))
 
             # Revaluacion
             for bom in done_ids:
                 self.env['mrp.bom'].browse(bom).action_reval()
+
+            _logger.warning(
+                _('GEB - termina de revaluar'))
