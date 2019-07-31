397c6143615067400c96a41979fdead9809543d6
deysy <damp_0508@hotmail.com>
2019-07-31 11:03:16 -0500

    FIX add segmentos relacionados de pedido que sustituye

--- sale_order_replace/models/sale_order_replace.py
+++ sale_order_replace/models/sale_order_replace.py
@@ -46,6 +46,7 @@ class SaleOrder(models.Model):
         remp_date = ''
         remp_ord = ''
         rel_seg = ''
+        rel_seg_repla = ''
         for res in order_ids:
             if not res.date_cancelled:
                 remp_date = '---'
@@ -59,28 +60,46 @@ class SaleOrder(models.Model):
                 rel_seg = '---'
             else:
                 rel_seg = res.related_segment
+            if not res.order_replaced.related_segment:
+                rel_seg_repla = '---'
+            else:
+                rel_seg_repla = res.order_replaced.related_segment
             table += """
-                <tr><td style="border-bottom: 1px solid silver;">%s</td>
-                    <td style="border-bottom: 1px solid silver;">%s</td>
-                    <td style="border-bottom: 1px solid silver;">%s</td>
-                    <td style="border-bottom: 1px solid silver;">%s</td>
-                    <td align="right" style="border-bottom: 1px solid silver;">
-                    %s</td></tr>
-            """ % (remp_date, res.partner_id.name, res.name, remp_ord, rel_seg)
+
+            <div>
+                <style type="text/css">
+                    tr {
+                    font-family: 'Arial';
+                        color: #000000;
+                    }
+                    strong {
+                        font-size: 14px;
+                    }
+                </style>
+            </div>
+                <tr align="center"><td style="border-bottom: 1px solid silver; font-size:12px;">%s</td>
+                    <td style="border-bottom: 1px solid silver; font-size:12px;">%s</td>
+                    <td style="border-bottom: 1px solid silver; font-size:12px;">%s</td>
+                    <td style="border-bottom: 1px solid silver; font-size:12px;">%s</td>
+                    <td style="border-bottom: 1px solid silver; font-size:12px;">%s</td>
+                    <td align="center" style="border-bottom: 1px solid silver; font-size:12px;">
+                    %s</td>
+                </tr>
+            """ % (remp_date, res.partner_id.name, res.name, rel_seg, remp_ord, rel_seg_repla)
         mail_obj = self.env['mail.mail']
         body_mail = u"""
-            <div summary="o_mail_notification" style="padding:0px; width:700px;
-             margin:0 auto; background: #FFFFFF repeat top /100%%; color:#77777
+            <div summary="o_mail_notification" style="padding:0px; width:100%%;
+             margin:0 auto; background: #FFFFFF repeat top  /100%%; color:#77777
              7">
-                <table cellspacing="0" cellpadding="0" style="width:700px;
+                <table cellspacing="0" cellpadding="0" style="width:900px;
                 border-collapse:collapse; background:inherit; color:inherit">
                     <tbody><tr>
-                        <td valign="center" width="270" style="padding:5px 10px
+                        <td valign="center" width="400" style="padding:5px 10px
                          5px 5px;font-size: 18px">
                             <p>Los siguientes pedidos fueron Cancelados y/o
                             Cerrados</p>
                         </td>
-                        <td valign="center" align="right" width="270"
+                        <td valign="center" align="right" width="400"
                         style="padding:5px 15px 5px 10px; font-size: 12px;">
                             <p>
                             <strong>Sent by</strong>
@@ -97,13 +116,13 @@ class SaleOrder(models.Model):
                     </tr>
                 </tbody></table>
             </div>
-            <div style="padding:0px; width:700px; margin:0 auto; background:
-            #FFFFFF repeat top /100%%; color:#777777">
+            <div style="padding:0px; width:900px; margin:0 auto; background:
+            #FFFFFF repeat top /900%%; color:#777777">
                 <table cellspacing="0" cellpadding="0" style="vertical-align:
                 top; padding:0px; border-collapse:collapse; background:inherit;
                  color:inherit">
                     <tbody><tr>
-                        <td valign="top" style="width:700px; padding:5px 10px
+                        <td valign="top" style="width:900px; padding:5px 10px
                         5px 5px; ">
                             <div>
                                 <hr width="100%%" style="background-color:
@@ -115,26 +134,29 @@ class SaleOrder(models.Model):
                     </tr></tbody>
                 </table>
             </div>
-            <div style="padding:0px; width:700px; margin:0 auto; background:
+            <div style="padding:0px; width:100%%; margin:0 auto; background:
             #FFFFFF repeat top /100%%;color:#777777">
                 <table style="border-collapse:collapse; margin: 0 auto; width:
-                700px; background:inherit; color:inherit">
+                1100px; background:inherit; color:inherit">
                     <tbody><tr>
-                        <th width="14%%" style="padding:5px 10px 5px 5px;font-
-                        size: 14px; border-bottom: 2px solid silver;"><strong>
+                        <th width="10%%" style="padding:5px 10px 5px 5px;font-
+                        size: 12px; border-bottom: 2px solid silver;"><strong>
                         Fecha de Cancelacion y/o Cerrado</strong></th>
-                        <th width="50%%" style="padding:5px 10px 5px 5px;font-
-                        size: 14px; border-bottom: 2px solid silver;"><strong>
+                        <th width="40%%" style="padding:5px 10px 5px 5px;font-
+                        size: 12px; border-bottom: 2px solid silver;"><strong>
                         Cliente</strong></th>
                         <th width="12%%" style="padding:5px 10px 5px 5px;font-
-                        size: 14px; border-bottom: 2px solid silver;"><strong>
+                        size: 12px; border-bottom: 2px solid silver;"><strong>
                         Pedido</strong></th>
-                        <th width="12%%" style="padding:5px 10px 5px 5px;font-
-                        size: 14px; border-bottom: 2px solid silver;"><strong>
-                        Pedido que Sustituye</strong></th>
-                        <th width="12%%" style="padding:5px 10px 5px 5px;font-
-                        size: 14px; border-bottom: 2px solid silver;"><strong>
+                        <th width="20%%" style="padding:5px 10px 5px 5px;font-
+                        size: 12px; border-bottom: 2px solid silver;"><strong>
                         Segmento(s) Relacionado(s)</strong></th>
+                        <th width="12%%" style="padding:5px 10px 5px 5px;font-
+                        size: 12px; border-bottom: 2px solid silver;"><strong>
+                        Pedido que Sustituye</strong></th>
+                        <th width="20%%" style="padding:5px 10px 5px 5px;font-
+                        size: 12px; border-bottom: 2px solid silver;"><strong>
+                        Seg. Rel. Pedido Sustituye</strong></th>
                     </tr>
                     %s
                     </tbody>
@@ -143,7 +165,7 @@ class SaleOrder(models.Model):
           """ % (self.env.user.company_id.name, table)
         mail = mail_obj.create({
             'subject': 'Pedidos Cancelados y/o Cerrados',
-            'email_to': 'equipocompras@gebesa.com,sistemas@gebesa.com,brandon.ramirez@gebesa.com,miguel.martinez@gebesa.com,jose.medina@gebesa.com,victor.campos@gebesa.com,elizabeth.valenzuela@gebesa.com,cristina.rodriguez@gebesa.com,julio.delarosa@gebesa.com',
+            'email_to': 'equipocompras@gebesa.com,sistemas@gebesa.com,brandon.ramirez@gebesa.com,miguel.martinez@gebesa.com,victor.campos@gebesa.com,elizabeth.valenzuela@gebesa.com,cristina.rodriguez@gebesa.com,julio.delarosa@gebesa.com',
             'headers': "{'Return-Path': u'odoo@gebesa.com'}",
             'body_html': body_mail,
             'auto_delete': True,
