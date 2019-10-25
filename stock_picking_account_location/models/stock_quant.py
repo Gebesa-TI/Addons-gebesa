f2b099d59d55283dbe7db62bf311da6057e33a2a
Samuel <samuel.barron@gebesa.com>
2019-10-25 09:17:09 -0500

    FIX

--- stock_picking_account_location/models/stock_quant.py
+++ stock_picking_account_location/models/stock_quant.py
@@ -352,9 +352,9 @@ class stock_quant(osv.osv):
             journal_id, acc_src, acc_dest, acc_valuation = self._get_accounting_data_for_valuation(cr, uid, move, context=ctx)
             self._create_account_move_line(cr, uid, quants, move, acc_src, acc_dest, journal_id, context=ctx)
 
-        if move.location_id.usage == 'internal' and move.location_dest_id.usage == 'internal' and 
+        if (move.location_id.usage == 'internal' and move.location_dest_id.usage == 'internal' and 
                 (move.location_id.stock_warehouse_id.id != move.location_dest_id.stock_warehouse_id.id or
-                    move.location_id.type_stock_loc != move.location_dest_id.type_stock_loc):
+                    move.location_id.type_stock_loc != move.location_dest_id.type_stock_loc)):
             ctx = context.copy()
             ctx['force_company'] = move.company_id.id
             journal_id, acc_src, acc_dest, acc_valuation = self._get_accounting_data_for_valuation(cr, uid, move, context=ctx)
